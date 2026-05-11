d0de970ad0aec2c2663977960d742d317f00c349
Minhyeok Park <pmh_only@pmh.codes>
2026-05-11 15:36:39 +0900

    docs: add detailed readme md file

---
+++ docs/render.mjs
@@ -0,0 +1,32 @@
+import fs from 'node:fs/promises'
+import process from 'node:process'
+import path from 'node:path'
+
+const cwd = process.cwd()
+const appPath = path.join(cwd, 'apps')
+const appNames = await fs
+  .readdir(appPath, { withFileTypes: true })
+  .then((v) => Promise.all(v
+    .filter((v) => v.isFile() && [".yaml", ".yml"].includes(path.extname(v.name)))
+    .map((v) => fs.readFile(path.join(appPath, v.name), 'utf-8'))))
+  .then((v) => v
+    .flatMap((v) => [...v.matchAll(/^\s*name:\s+(\S+)\n[^\n]*?^\s*namespace:/gm)]
+    .map((v) => v[1])))
+
+const docsPath = path.join(cwd, 'README.md')
+const docs = await fs.readFile(docsPath, 'utf-8')
+
+await fs.writeFile(docsPath, docs
+  .replaceAll(
+    /<!-- ?SYNC_TABLE_START ?-->[\s\S]*<!-- ?SYNC_TABLE_END ?-->/gm,
+    `<!-- SYNC_TABLE_START -->
+
+<table>
+  ${appNames.map((v, i) => `
+  ${i % 6 === 0 ? '<tr>' : ''}
+  <td><img src="https://argo.pmh.codes/api/badge?name=${v}&revision=false&showAppName=false" width="100px" /><br /><code>${v}</code></td>
+  ${i % 6 === 5 ? '</tr>' : ''}
+  `).join('')}
+</table>
+
+<!-- SYNC_TABLE_END -->`))
